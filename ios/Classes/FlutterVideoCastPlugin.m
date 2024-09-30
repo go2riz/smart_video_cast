3c1bf80508870b23bbd051e1fd49a3f069de6eed
Haider <go2riz@gmail.com>
2024-09-30 14:33:42 +0500

    Fix plugin name

--- ios/Classes/FlutterVideoCastPlugin.m
+++ ios/Classes/FlutterVideoCastPlugin.m
@@ -1,4 +1,4 @@
-#import "FlutterVideoCastPlugin.h"
+#import "SmartVideoCastPlugin.h"
 #if __has_include(<smart_video_cast/smart_video_cast-Swift.h>)
 #import <smart_video_cast/smart_video_cast-Swift.h>
 #else
@@ -8,8 +8,8 @@
 #import "smart_video_cast-Swift.h"
 #endif
 
-@implementation FlutterVideoCastPlugin
+@implementation SmartVideoCastPlugin
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
-  [SwiftFlutterVideoCastPlugin registerWithRegistrar:registrar];
+  [SwiftSmartVideoCastPlugin registerWithRegistrar:registrar];
 }
 @end
--- ios/Classes/SwiftFlutterVideoCastPlugin.swift
+++ ios/Classes/SwiftFlutterVideoCastPlugin.swift
@@ -1,7 +1,7 @@
 import Flutter
 import UIKit
 
-public class SwiftFlutterVideoCastPlugin: NSObject, FlutterPlugin {
+public class SwiftSmartVideoCastPlugin: NSObject, FlutterPlugin {
     public static func register(with registrar: FlutterPluginRegistrar) {
         let factory = AirPlayFactory(registrar: registrar)
         let chromeCastFactory = ChromeCastFactory(registrar: registrar)
